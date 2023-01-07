cdf3bd282052d896a08fe2305b8f9a71bcf49c7f
evverenn <jokerj224@gmail.com>
2023-01-07 22:24:56 +0300

    develop. Int init done.

---
+++ src/source/c_stack_int/int_c_stack_init.c
@@ -0,0 +1,14 @@
+#include "c_stack_int.h"
+int int_c_stack_init(size_t size, c_stack_int* stack) {
+  if (!stack) return C_STACK_NULL_REF;
+  if (size >= C_STACK_MAX_SIZE) return C_STACK_BAD_SIZE;
+  stack->aloc_size = !size ? C_STACK_BASE_SIZE : size;
+  stack->values = calloc(stack->aloc_size, sizeof(int));
+  if (!stack->values) {
+    stack->length = 0;
+    stack->aloc_size = 0;
+    return C_STACK_NULL_REF;
+  }
+  stack->length = 0;
+  return C_STACK_OK;
+}
