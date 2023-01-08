7842ef8df974ca98a46cbe62f7b976ccf276899c
evverenn <jokerj224@gmail.com>
2023-01-08 20:06:03 +0300

    develop. Added color defines for tests.

---
+++ src/include/c_stack_colors.h
@@ -0,0 +1,15 @@
+#ifndef C_STACK_COLORS_H_
+#define C_STACK_COLORS_H_
+#define RED "\033[0;31m"
+#define BRED "\033[1;31m"
+#define URED "\033[4;31m"
+#define RESET "\033[0m"
+#define REDB "\033[41m"
+#define REDHB "\033[0;101m"
+#define HRED "\033[0;91m"
+#define BHRED "\033[1;91m"
+#define GRN "\033[0;92m"
+#define UGRN "\033[4;32m"
+#define ENDCOLOR "\033[0m"
+#define PRETTY_PRINT(name) (UGRN name ENDCOLOR)
+#endif  // C_STACK_COLORS_H_
