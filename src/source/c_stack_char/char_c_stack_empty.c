09a09c59f6d8778e838dab4ef073824c2569bbb4
evverenn <jokerj224@gmail.com>
2023-01-14 20:01:30 +0300

    develop. Char empty conditional jump fix.

--- src/source/c_stack_char/char_c_stack_empty.c
+++ src/source/c_stack_char/char_c_stack_empty.c
@@ -1,5 +1,8 @@
 #include "c_stack_char.h"
-bool char_c_stack_empty(c_stack_char* stack) { 
-  if (!char_c_stack_valid(stack)) return true;
-  return stack->length ? false : true;
+#include "stdio.h"
+bool char_c_stack_empty(c_stack_char* stack) {
+  if (!char_c_stack_valid(stack)) return false;
+  bool res = false;
+  res = stack->length ? false : true;
+  return res;
 }
