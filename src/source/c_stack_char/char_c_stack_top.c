c1241fe748fb037fb308a0d08ab642f28e220c37
evverenn <jokerj224@gmail.com>
2023-01-14 20:01:52 +0300

    develop. Char top empty stack bug fix.

--- src/source/c_stack_char/char_c_stack_top.c
+++ src/source/c_stack_char/char_c_stack_top.c
@@ -1,5 +1,6 @@
 #include "c_stack_char.h"
 char* char_c_stack_top(c_stack_char* stack) {
   if (!char_c_stack_valid(stack)) return NULL;
+  if (char_c_stack_empty(stack)) return NULL;
   return &stack->values[stack->length - 1];
 }
