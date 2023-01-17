48046845d3849b94410181a20ff12c4abb77bcd8
evverenn <jokerj224@gmail.com>
2023-01-17 19:20:19 +0300

    develop. Double / Int Empty() error return fix.

--- src/source/c_stack_double/double_c_stack_empty.c
+++ src/source/c_stack_double/double_c_stack_empty.c
@@ -1,6 +1,6 @@
 #include "c_stack_double.h"
 bool double_c_stack_empty(c_stack_double* stack) {
-  if (!double_c_stack_valid(stack)) return true;
+  if (!double_c_stack_valid(stack)) return false;
   bool res = false;
   res = stack->length ? false : true;
   return res;
--- src/source/c_stack_int/int_c_stack_empty.c
+++ src/source/c_stack_int/int_c_stack_empty.c
@@ -1,6 +1,6 @@
 #include "c_stack_int.h"
 bool int_c_stack_empty(c_stack_int* stack) {
-  if (!int_c_stack_valid(stack)) return true;
+  if (!int_c_stack_valid(stack)) return false;
   bool res = false;
   res = stack->length ? false : true;
   return res;
